0c82964743e29ba5ca0ddc888ec497370fb07c66
rkghostivy <rukny.kawar@ghost-ivy.com>
2024-09-12 18:50:27 +0300

    small middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("auth-token");
 
   if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   return NextResponse.next();
